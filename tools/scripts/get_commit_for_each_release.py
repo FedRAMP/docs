ea5569925f6a687968fa27c88f624f80ca857f84
dan-fedramp <dan.chandler@gsa.gov>
2025-12-10 12:14:14 -0500

    Refactor `get_commit_for_each_release.py`: add support for `until_date`, improve GitHub link generation, and enhance commit lookup logic.

--- tools/scripts/get_commit_for_each_release.py
+++ tools/scripts/get_commit_for_each_release.py
@@ -1,33 +1,59 @@
 import os
 import subprocess
 import json
+from datetime import datetime, timedelta
 
 
-def find_earliest_commit(file_path, search_text):
+def find_earliest_commit(file_path, search_text, until_date=None):
     try:
-        cmd = [
-            'git', 'log', '-S', search_text,
-            '--reverse', '--format=%H',
-            '--', file_path
-        ]
-
-        print(f"Running command: {' '.join(cmd)}")
-
-        result = subprocess.check_output(cmd, text=True)
+        result = git_log_search(file_path, search_text, until_date)
 
         if result:
             # The first line is the earliest commit
-            earliest_commit = result.strip().split('\n')[0]
-            print(f"Found: {earliest_commit}")
-            return earliest_commit
+            earliest_commit_link = build_github_link(result, 'root')
+            return earliest_commit_link
         else:
             print(f"Text '{search_text}' not found in the history of {file_path}")
-            return None
+            print("Checking previous file locations...")
+            # FRMR JSON files used to be in a data subdirectory.
+            file_path = os.path.join(repo_root, "data", file_name)
+            result = git_log_search(file_path, search_text, until_date)
+            if result:
+                # The first line is the earliest commit
+                earliest_commit_link = build_github_link(result, 'data')
+                return earliest_commit_link
+            else:
+                print("Text not found in any known location.")
+                return None
 
     except subprocess.CalledProcessError as e:
         print(f"Git command failed: {e}")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+
+def build_github_link(commit_hash: str, file_location: str = 'root'):
+    earliest_commit = commit_hash.strip().split('\n')[0]
+    print(f"Found: {earliest_commit}")
+    if file_location == 'root':
+        earliest_commit_link = f'https://raw.githubusercontent.com/FedRAMP/docs/{earliest_commit}/{file_name}'
+    elif file_location == 'data':
+        earliest_commit_link = f'https://raw.githubusercontent.com/FedRAMP/docs/{earliest_commit}/data/{file_name}'
+    else:
+        print(f"Invalid file location: {file_location}")
+        return None
+    return earliest_commit_link
+
+
+def git_log_search(file_path, search_text, until_date) -> str:
+    search_date = (datetime.strptime(until_date, "%Y-%m-%d") + timedelta(days=1)).strftime("%Y-%m-%d")
+    cmd = ['git', 'log', '--follow', '-S', search_text, f'--until="{search_date}"', '--reverse', '--format=%H', '--', file_path]
+    # cmd = ['git', 'log', '--follow', '-S', search_text, '--reverse', '--format=%H', '--', file_path]
+
+    print(f"Running command: {' '.join(cmd)}")
+
+    result = subprocess.check_output(cmd, text=True)
+    return result
 
 
 # Example usage for your specific file
@@ -46,8 +72,10 @@ for file in os.listdir(repo_root):
             content = json.load(f)
             for release in content["info"]["releases"]:
                 text_to_find = release["id"]
-                earliest_commit_hash = find_earliest_commit(os.path.join(repo_root,file_name), text_to_find)
-                release["machine_readable_link"] = f'https://raw.githubusercontent.com/FedRAMP/docs/{earliest_commit_hash}/{file_name}'
+                until_date = release["published_date"]
+                
+                earliest_commit_link = find_earliest_commit(os.path.join(repo_root, file_name), text_to_find, until_date)
+                release["machine_readable_link"] = earliest_commit_link
                 release_list.append(release)
             content["info"]["releases"] = release_list
             with open(os.path.join(repo_root,file_name), 'w') as outfile:
